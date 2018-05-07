adde891cf772ee29d460742c2eef9c844fd6e545
Emily Zhao <tong.zhao.92@gmail.com>
2018-05-07 21:27:00 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -19,4 +19,11 @@ class App < Sinatra::Base
     "Goodbye, #{@user_name}."
   end
 
+  get "/multiply/:num1/:num2" do
+    num1 = params[:num1]
+    num2 = params[:num2]
+    a = num1 * num2
+    "#{num1} times #{num2} equals #{num1}"
+  end
+
 end
